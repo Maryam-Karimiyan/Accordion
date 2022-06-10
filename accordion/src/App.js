74b0ec0b885f113dfa1c77629ed5a576b1fe07b8
maryam <Ofogh.maryam14@gmail.com>
2022-06-10 12:10:41 +0430

    commit

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,19 +1,36 @@
-import  Accordion  from "./components/Accordion";
 import  Header from './components/accordionHeader';
 // import {ThemeProvider} from 'styled-components';
-import LastOne from "./components/LastOne";
-import { useState } from "react";
+// import LastOne from "./components/LastOne";
+import Form from './components/Form';
 import "./App.css";
 
-
+const theme = {
+  bgform: "#fff",
+  borderform: "#D2D2D2",
+  borderRadiosform: "5px",
+  paddingform: "20px",
+  mdspaing: "20px",
+  smspacing: "5px",
+  borderinput: "#9747FF",
+  bgbutton: "#FFCD29",
+  colorbtn:'#333',
+};
+const types={
+  email:'email',
+  submit:'submit'
+}
+const action={
+  get:'get',
+  post:'post'
+}
 //
 function App() {
 
   return (
     <div className="App">
       <Header bg="#fff" textcolor="#9747FF" />
-      {/* <Accordion></Accordion> */}
-      <LastOne  detailbg="#9747FF" summarybg="#FFCD29" palign='left'/>
+      {/* <LastOne  detailbg="#9747FF" summarybg="#FFCD29" palign='left'/> */}
+      <Form btnText='Sign In' action={action} theme={theme} types={types} />
     </div>
   );
 }
---
+++ accordion/src/components/Form.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const SForm = styled.form`
+  width: 500px;
+  text-align: center;
+  margin: auto;
+  background: ${({ theme }) => theme.bgform};
+  border-radius: ${({ theme }) => theme.borderRadiosform};
+  padding: ${({ theme }) => theme.paddingform};
+`;
+export const SFromTitle=styled.span`
+    font-size: 24px;
+    font-weight: 600;
+`
+export const SFormControl = styled.div`
+  :first-of-type {
+    margin-top: ${({ theme }) => theme.mdspaing};
+  }
+  :not(:last-of-type) {
+    margin-bottom: ${({ theme }) => theme.smspacing};
+  }
+`;
+export const SLable = styled.label`
+  display: block;
+  font-size: 14px;
+  font-weight: 600;
+  margin-left: 4px;
+  margin-bottom: calc(${({ theme }) => theme.smspacing/4});
+`;
+export const SInput = styled.input`
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.borderinput};
+  width: 100%;
+  padding: ${({ theme }) => theme.smspacing};
+  font-size: 14px;
+  border-radius: ${({ theme }) => theme.borderRadiosform};
+`;
+export const SButton = styled.button`
+  width: 100%;
+  background: ${({ theme }) => theme.bgbutton};
+  color: ${({ theme }) => theme.colorbtn};
+  padding: ${({ theme }) => theme.smspacing};
+  display: flex;
+  justify-content: center;
+  border-radius: ${({ theme }) => theme.borderRadiosform};
+  margin-top: ${({ theme }) => theme.mdspaing};
+  cursor: pointer;
+`;
+
+
+
+const Form = ({ action, titleText, labelText,btnText,theme,types }) => {
+    const handleSubmit=()=>{
+        console.log('Submitted')
+    }
+  return (
+    <SForm action={action.get} theme={theme} onSubmit={handleSubmit}>
+      <SFromTitle>{titleText}</SFromTitle>
+      <SFormControl theme={theme}>
+        <SLable theme={theme}>{labelText}</SLable>
+        <SInput type={types.email} theme={theme}/>
+      </SFormControl>
+      <SButton theme={theme}>{btnText}</SButton>
+    </SForm>
+  );
+};
+ 
+export default Form;
